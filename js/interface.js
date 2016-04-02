48d5c0b7533c96228bcadf7abe89850cb3c81b6e
CharlesAMoss <charlesamoss@yahoo.com>
2016-04-01 22:54:05 -0700

    add click discard to ui

--- js/interface.js
+++ js/interface.js
@@ -5,20 +5,32 @@ const startNewGame = () => {
   return dealCards(shuffleDeck(getDeck()));
 };
 
+// const checkExist = setInterval(function () {
+//   if ($('span.held')) {
+//     console.log('Exists!');
+//     clearInterval(checkExist);
+//   }
+// }, 100);
+
 $(document).ready(function () {
   let dealtCards = startNewGame();
+
+  let turn = 0;
+  let player = [];
+
+  (turn % 2 === 0) ? (player = pOne, turn += 1) : (player = pTwo, turn += 1);
+  console.log(player);
 
   let drawPile = document.createElement('img');
   drawPile.className = 'cardBack';
   drawPile.src = cardBack;
-  drawPile.onclick = () => drawCard();
+  drawPile.onclick = () => drawCard(player);
 
   let discardPile = document.createElement('div');
   let discardColor = suitColor(_.last(discard));
   discardPile.className = discardColor;
   discardPile.className += ' discard';
   discardPile.appendChild(document.createTextNode(_.last(discard)));
-  discardPile.onclick = () => console.log('discard click');
 
   let mainPiles = document.createElement('div');
   mainPiles.className = 'mainPiles';
@@ -30,14 +42,34 @@ $(document).ready(function () {
 
   let playerTwo = document.createElement('div');
   playerTwo.className = 'playerTwo';
-  console.log(playerTwo);
+
+  let discardButton = document.createElement('button');
+  discardButton.className = 'btn';
+  discardButton.appendChild(document.createTextNode('discard'));
+
+  let winButton = document.createElement('button');
+  winButton.className = 'btn';
+  winButton.appendChild(document.createTextNode('Yaniv'));
+  winButton.onclick = console.log('It is not that easy');
 
   $('#main').append(mainPiles, playerOne, playerTwo);
   $('.playerOne').html(printCard(pOne));
   $('.playerTwo').html(printCard(pTwo));
+  $('#aside').append(discardButton, winButton);
 
-  $('.playerOne').children().on('click', function () {
-    $(this).toggleClass('held');
-    console.log($(this).text());
+  $('.playerOne').children().on('click', function (event) {
+    if ($(this).hasClass('held')) {
+      let heldCard = $(this).text();
+      discardCard(heldCard, player);
+      $('.discard').html(document.createTextNode(_.last(discard)));
+      let currentSuit = suitColor(_.last(discard));
+      $('.discard').removeClass().addClass(currentSuit);
+      discardPile.className += ' discard';
+      $('.playerOne').html(printCard(pOne));
+    } else {
+      $(this).toggleClass('held');
+      console.log($(this).text());
+    };
   });
+
 });
